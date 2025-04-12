8913ab312460fa55ad2ea09a68cf38b0825b4958
huangruohao <vic_hrh@foxmail.com>
2025-04-12 09:15:39 +0800

    refactor(基础知识): 重构3_5.py 示例代码
    - 移除了原始代码中的变量声明和类型检查部分
    - 添加了字符串格式化和输入示例代码
    - 增加了转义字符和 end 参数的使用示例
    - 引入了新的变量类型示例，包括整数、浮点数和布尔值- 展示了不同格式化方法的使用，如 % 格式化和 format() 方法

--- 基础知识/3_5.py
+++ 基础知识/3_5.py
@@ -1,29 +1,25 @@
-num1 = True
-num2 = 10
+# 输入
+# input_str = input("请输入一个字符串：")
+# print("您刚才输入的数据类型为:",type(input_str))
+# print("您输入的字符串为:",input_str)
 
-print(type(num1))
-print(type(num2))
+print("hello Python")
+print("hello","Python")
 
-print(type(num1) == type(num2))
-print(isinstance(num1, int))
-print(isinstance(num2, int))
+print("hello\\n 结尾",end="\n")
 
-print(isinstance(num1, bool))
-print(isinstance(num2, bool))
+print("hello\\n 结尾",end="")
+print("world")
 
-# String 类型的
-print("""########### String ###########""")
+int1 = 10
+float1 = 10.12345
+# str1 = "int1 = %d,float1 = %f" %(int1,float1)
+# print(str1)
+bool1 = True
 
-str1 = '你好中国'
-print(str1)
-print(type(str1))
-
-byte1 = str1.encode(encoding='utf-8')
-print(byte1)
-
-print(type(byte1))
-
-str2 = byte1.decode(encoding='utf-8')
+str2 = "int1 = {},float1 = {},bool1 = {}".format(int1,float1,bool1)
 print(str2)
 
 
+str3 = "{:0<20,.2f}".format(123456789.123456789)
+print(str3)
